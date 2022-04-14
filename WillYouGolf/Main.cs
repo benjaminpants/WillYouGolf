967f91c52c6f1bf30f6a81f14ad9b9203f3057c2
benjaminpants <maltbyfamily@gmail.com>
2022-04-14 00:21:45 -0700

    update to latest gmml

--- WillYouGolf/Main.cs
+++ WillYouGolf/Main.cs
@@ -15,18 +15,14 @@ namespace WillYouGolf
 {
     public class GameMakerMod : IGameMakerMod
     {
-        public static int golfhitsound = -1;
-
         public static Dictionary<string, string> GMLkvp = new Dictionary<string, string>();
         
 
-        public void Load(int audioGroup, ModData currentmod)
+        public void Load(int audioGroup, UndertaleData data, ModData currentmod)
         {
-            UndertaleData data = Patcher.data;
-
             string soundsfolder = Path.Combine(currentmod.path, "Sounds");
-            Conviences.AddAudioFolder(audioGroup, 1, soundsfolder);
-            Conviences.AddAudioFolder(audioGroup, 2, Path.Combine(soundsfolder, "Music"));
+            Conviences.AddAudioFolder(audioGroup, 1, soundsfolder, data);
+            Conviences.AddAudioFolder(audioGroup, 2, Path.Combine(soundsfolder, "Music"), data);
             if (audioGroup != 0) return;
             string gmlfolder = Path.Combine(currentmod.path, "GMLSource");
             string charactersfolder = Path.Combine(currentmod.path, "Characters");
@@ -37,13 +33,13 @@ namespace WillYouGolf
             WYSCustomCharacterAPI.GameMakerMod.DisabledIDs.Add("brokendrone");
             WYSCustomCharacterAPI.GameMakerMod.LoadCustomCharacters(charactersfolder);
             data.GameObjects.ByName("obj_player").EventHandlerFor(EventType.Draw, EventSubtypeDraw.DrawGUI, data.Strings, data.Code, data.CodeLocals)
-                .AppendGMLSafe(GMLkvp["DrawPuckGUI"]);
+                .AppendGmlSafe(GMLkvp["DrawPuckGUI"], data);
             data.GameObjects.ByName("obj_music_credits").EventHandlerFor(EventType.Other, EventSubtypeOther.User0, data.Strings, data.Code, data.CodeLocals)
-                .AppendGMLSafe("play_music = mus_GOLFCREDITS");
+                .AppendGmlSafe("play_music = mus_GOLFCREDITS", data);
 
-            Hooker.HookCode("gml_Object_obj_input_overrider_end_Other_10", GMLkvp["obj_input_overrider_end_Other_10"]);
+            data.HookCode("gml_Object_obj_input_overrider_end_Other_10", GMLkvp["obj_input_overrider_end_Other_10"]);
 
-            Hooker.HookFunction("scr_ai_set_difficulty_multi", 
+            data.HookFunction("scr_ai_set_difficulty_multi", 
             @"
             #orig#()
             setting_air_cat_probability = 0
